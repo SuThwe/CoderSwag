462334b14f73205af988fc793a9cff400b1f9209
Su Thwe Thwe Tun <suthwe@Sus-MacBook-Pro.local>
2020-01-22 23:47:10 +0800

    lesson02

--- app/src/main/java/com/purple/su/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/purple/su/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,21 @@ package com.purple.su.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.purple.su.coderswag.Model.Category
 import com.purple.su.coderswag.R
+import com.purple.su.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.Categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/purple/su/coderswag/Model/Category.kt
+++ app/src/main/java/com/purple/su/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.purple.su.coderswag.Model
 /**
  * Created by Su Thwe on 2020-01-20.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
